069d80d184c9148d874a7d4c743dedfb08e2f20a
Fournext <sebastianzc04@gmail.com>
2025-11-11 23:02:33 -0400

    fix: despliegue con redis server

--- back_generador_bd/diagramador_uml/settings.py
+++ back_generador_bd/diagramador_uml/settings.py
@@ -63,7 +63,7 @@ CHANNEL_LAYERS = {
             # "hosts": [("redis_server", 6379)],
 
             # el nombre del servicio en docker-compose
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis_server", 6379)],
         },
     },
 }
